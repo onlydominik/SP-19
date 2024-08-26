417696a9f6b34fd4f0e9a6f42ec3664ad583591b
Dominik <113696375+onlydominik@users.noreply.github.com>
2024-08-26 22:30:33 +0200

    Ready layout

---
+++ main.js
@@ -0,0 +1,7 @@
+ 
+let inputDay = 24//document.querySelector("#day");
+let inputMonth = 8//document.querySelector("#month");
+let inputYear = 1984//document.querySelector("#year");
+
+
+console.log(moment().format('dddd'));
