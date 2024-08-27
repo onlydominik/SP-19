08929df15b7a525b9e40aadc26cfd44d2f8f1e2e
Dominik <113696375+onlydominik@users.noreply.github.com>
2024-08-27 23:44:05 +0200

    Lib add - validation partial

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
- 
-let inputDay = 24//document.querySelector("#day");
-let inputMonth = 8//document.querySelector("#month");
-let inputYear = 1984//document.querySelector("#year");
 
 
-console.log(moment().format('dddd'));
+document.querySelector("button").addEventListener("click", () => {
+    
+    let inputDay = document.querySelector("#day").value;
+    let inputMonth = document.querySelector("#month").value;
+    let inputYear = document.querySelector("#year").value;
+    let error = false;
+ let labels = document.querySelectorAll("label");
+ labels.forEach(elem => {
+    if(!elem.querySelector("input").value) elem.querySelector("p").textContent = "This field is required"
+    else elem.querySelector("p").textContent = ""
+
+ })
+
+
+
+    if(inputDay, inputMonth, inputYear) {
+
+
+        let inputDate = moment([inputYear, inputMonth, inputDay]);
+        let diff = moment.preciseDiff(inputDate, moment(), true);
+        document.querySelector(".calculator__results p:nth-child(1) strong").textContent = diff["years"];
+        document.querySelector(".calculator__results p:nth-child(2) strong").textContent = diff["months"];
+        document.querySelector(".calculator__results p:nth-child(3) strong").textContent = diff["days"];
+    }
+})
+
+
+
+
+
+
